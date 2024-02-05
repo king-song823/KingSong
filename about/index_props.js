a19fb932059f0f2ff0be7a383e6868ef13bd2e11
king-song823 <king-song823@users.noreply.github.com>
2024-02-05 13:57:02 +0000

    deploy: 69f954e152555af9a776a891bef07ba05ce65f94

--- about/index_props.js
+++ about/index_props.js
@@ -8,7 +8,7 @@ export default {
     'outputPath': "about/index.html",
     'title': "About",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>About</h1>\n<p>The about page.</p>'
+            __html: '<h1>About</h1>\n<p>The about page.\n方式了</p>'
         } }),
     'head': React.createElement(React.Fragment, null,
         React.createElement("link", { crossOrigin: "anonymous", href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css", integrity: "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X", rel: "stylesheet" })),
@@ -21,7 +21,7 @@ export default {
         React.createElement("a", { href: "https://github.com/xcatliu/pagic", target: "_blank" }, "Pagic")),
     'contentTitle': React.createElement("h1", { key: "0" }, "About"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<p>The about page.</p>'
+            __html: '<p>The about page.\n方式了</p>'
         } }),
     'toc': null,
     'author': "ink-song",
@@ -29,8 +29,8 @@ export default {
         "ink-song"
     ],
     'date': "2024-02-05T09:41:51.000Z",
-    'updated': null,
-    'excerpt': "The about page.",
+    'updated': "2024-02-05T13:56:31.000Z",
+    'excerpt': "The about page. 方式了",
     'cover': undefined,
     'sidebar': [
         {
